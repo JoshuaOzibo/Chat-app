9f6383f5c0ad1d95faee88a3ac4a54651c863a10
JoshuaOzibo <joshuamichaelozibo@gmail.com>
2025-04-08 21:17:49 +0100

    Add @radix-ui/react-slot dependency and update ChatSidebar and ChatWindow components

--- src/components/ChatSidebar/ChatSidebar.tsx
+++ src/components/ChatSidebar/ChatSidebar.tsx
@@ -1,9 +1,28 @@
 const ChatSidebar = () => {
   return (
     <>
-      
-    </>
-  )
-}
+      <section>
+        {/*  */}
+        <main>
+          <div>
+            <h1>Recent Chats</h1>
+            <p>Chat from your friends 😘</p>
+          </div>
 
-export default ChatSidebar
+          {/* icon */}
+        </main>
+
+        <main>
+          <div>
+            <h2>Chat</h2>
+            <p>Start New Conversation</p>
+          </div>
+
+          {/* icon */}
+        </main>
+      </section>
+    </>
+  );
+};
+
+export default ChatSidebar;
---
+++ src/components/ChatWindow/CurrentUserId.tsx
@@ -0,0 +1,20 @@
+import { Badge } from "@/components/ui/badge";
+
+export default function CurrentUserId() {
+  return (
+    <>
+      <section>
+        <main>
+          {/* <img src="" alt="" /> */}
+          <h1>Josephin water</h1>
+          <Badge variant="outline">Active</Badge>
+        </main>
+
+
+        <main>
+            
+        </main>
+      </section>
+    </>
+  );
+}
