735477beef73b9b60925c55594316291dee1cf0b
JoshuaOzibo <joshuamichaelozibo@gmail.com>
2025-04-14 21:56:37 +0100

    updated icons code

--- src/components/ChatWindow/CurrentChattingUser.tsx
+++ src/components/ChatWindow/CurrentChattingUser.tsx
@@ -1,11 +1,12 @@
 import { Badge } from "@/components/ui/badge";
 import {FaVolumeHighIcon} from '@/Icons/Icons'
+import {FiSearchIcon, IoCallOutlineIcon, MdOutlineVideocamIcon, AiOutlineAppstoreIcon, CiMenuKebabIcon} from '@/Icons/Icons'
 
 
 export default function CurrentChattingUser() {
   return (
     <>
-      <section className="bg-blue-500 flex absolute justify-between items-center w-[90%] py-6 m-auto px-10">
+      <section className="bg-blue-500 flex absolute justify-between items-center lg:w-[70%] w-[90%] py-6 px-10 top-5 left-1/2 -translate-x-1/2">
         <main>
           {/* <img src="" alt="" /> */}
           <h1>Josephin water</h1>
@@ -16,13 +17,13 @@ export default function CurrentChattingUser() {
         <main className="bg-red-500 flex justify-between items-center w-1/2">
           <div className="flex gap-2 items-center">
            <FaVolumeHighIcon />
-           <FaVolumeHighIcon />
+           <FiSearchIcon />
           </div>
           <div className="flex gap-2 items-center">
-          <FaVolumeHighIcon />
-          <FaVolumeHighIcon />
-          <FaVolumeHighIcon />
-          <FaVolumeHighIcon />
+          <IoCallOutlineIcon />
+          <MdOutlineVideocamIcon />
+          <AiOutlineAppstoreIcon />
+          <CiMenuKebabIcon />
           </div>
         </main>
       </section>
