7c3c7b0553e1382ebab6d260026fe31904b20b9b
JoshuaOzibo <joshuamichaelozibo@gmail.com>
2025-04-14 21:39:44 +0100

    Add new icons to Icons.ts for enhanced UI components including call, video, kebab menu, microphone, and plus icons.

--- src/Icons/Icons.ts
+++ src/Icons/Icons.ts
@@ -2,6 +2,11 @@ import { AiOutlineAppstore } from "react-icons/ai";
 import { FiSearch } from "react-icons/fi";
 import { FaVolumeHigh } from "react-icons/fa6";
 import { IoMdPaperPlane } from "react-icons/io";
+import { IoCallOutline } from "react-icons/io5";
+import { MdOutlineVideocam } from "react-icons/md";
+import { CiMenuKebab } from "react-icons/ci";
+import { TiMicrophoneOutline } from "react-icons/ti";
+import { TiPlus } from "react-icons/ti";
 
 // Grid menu Icon
 export const AiOutlineAppstoreIcon =  AiOutlineAppstore;
@@ -13,3 +18,18 @@ export const FaVolumeHighIcon =  FaVolumeHigh;
 
 //plane icon
 export const IoMdPaperPlaneIcon =  IoMdPaperPlane;
+
+//call
+export const IoCallOutlineIcon =  IoCallOutline;
+
+//video call 
+export const MdOutlineVideocamIcon =  MdOutlineVideocam;
+
+// kebab menu
+export const CiMenuKebabIcon =  CiMenuKebab;
+
+// microphone
+export const TiMicrophoneOutlineIcon =  TiMicrophoneOutline;
+
+//Plus
+export const TiPlusIcon =  TiPlus;
