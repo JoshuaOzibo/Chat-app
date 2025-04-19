2e2da8a52b9cad8921f8d0a4f294f91b9bda8505
JoshuaOzibo <joshuamichaelozibo@gmail.com>
2025-04-19 19:38:15 +0100

    updated message bubbles

--- src/components/ChatWindow/messageBubbles/ReceiverMessageBubble.tsx
+++ src/components/ChatWindow/messageBubbles/ReceiverMessageBubble.tsx
@@ -19,7 +19,7 @@ export default function ReceiverMessageBubble({ senderName, message }: MessageTy
     <div className="flex items-start justify-start">
       <div className="max-w-[70%]">
         <div className="bg-gray-200 rounded-bl-2xl rounded-r-2xl px-4 py-2 text-gray-800">
-          <div className="flex justify-between items-center mb-1">
+          <div className="flex justify-between items-center mb-1 space-x-3">
             <p className="text-sm text-gray-600">{senderName}</p>
             <p className="text-xs text-gray-500">{messageTime}</p>
           </div>
